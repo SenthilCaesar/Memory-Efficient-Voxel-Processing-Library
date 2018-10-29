a600ae5d0a16c7177f90be2316033dca2daf472f
Senthil Palanivelu <senthilcaesar@gmail.com>
2018-10-29 15:44:35 -0400

    Ghost Cell Trimming

--- src/Main.py
+++ src/Main.py
@@ -4,10 +4,14 @@ import numpy as np
 if __name__ == '__main__':
     
     # Parameters
-    filename = 'gyroidUniform.npy'
+    #filename = 'gyroidUniform.npy'    
+    #one = np.random.uniform(size=(175,10000))
+    #one = one.reshape(175,100,100)
+    #np.save('175_100_100.npy', one)
     dimension = [200, 200, 200]
-    a = np.random.uniform(size=(3,9))
-    structure = a.reshape(3,3,3)
+    filename = 'gyroidUniform.npy'
+    #a = np.random.uniform(size=(3,9))
+    structure = np.ones((3,3,3))
     operation = 'dilation'
     
     # Main
@@ -18,3 +22,4 @@ if __name__ == '__main__':
     CRS = data.load_compressed()    
     data.Morphology(CRS, no_of_blocks, operation)
     data.merge_blocks()
+    
--- src/VoxelProcessing.py
+++ src/VoxelProcessing.py
@@ -10,7 +10,7 @@ class VoxelProcessing:
         self.y_dim = dimension[1]
         self.z_dim = dimension[2]
         self.struct_element = structure
-        self.arr_map = np.load('gyroidUniform.npy', mmap_mode="r")
+        self.arr_map = np.load(filename, mmap_mode="r")
         
     def print_arr_shape(self):
         print(self.arr_map.shape)
@@ -58,32 +58,40 @@ class VoxelProcessing:
 
     def Morphology(self, CRS, n_blocks, operation):
         start_index = 0
-        end_index = int(CRS.shape[1] / n_blocks)
+        fake_ghost = 1
+        splits = (self.arr_map.shape[0]*
+                    self.arr_map.shape[1])//(self.y_dim*10)
+        x = CRS.shape[1] // (splits*10)
+        end_index = int(CRS.shape[1] / n_blocks) + (x * fake_ghost)
         jump = int(CRS.shape[1] / n_blocks)
         self.f_handle = open('output/binary', 'wb')
         for i in range(0, n_blocks):
-            # print("Start Index = ",start_index)
-            # print("End Index = ", end_index)
+            print("Start Index = ",start_index)
+            print("End Index = ", end_index)
             block_2d = CRS[:,start_index:end_index].toarray()
+            print("Block Shape = ", block_2d.shape)
             # print("Block_2d Memory size = ", getsizeof(block_2d))
             # print("Block_2d Type = ", type(block_2d))
             block_2d = block_2d.T
-            start_index = end_index
-            end_index = end_index + jump
-            self.convert_to_3d(i, block_2d, operation)
+            start_index = end_index - (x * fake_ghost * 2)
+            if i == n_blocks -2:
+               end_index = end_index + jump - x
+            else:
+                end_index = end_index + jump
+            self.convert_to_3d(i, block_2d, operation, n_blocks, fake_ghost)
         self.f_handle.close()
             
-    def convert_to_3d(self, i, block_2d, operation):
-        divide = [5, 10, 15, 20]
-        arr_int = []
-        
-        for k in range(0, len(divide)):
-            value = block_2d.shape[0] / divide[k]
-            if float(value).is_integer():
-                arr_int.append(divide[k])
-        n_splits = arr_int[-1]       
-        
-        #print(n_splits, block_2d.shape)
+    def convert_to_3d(self, i, block_2d, operation, n_blocks, fake_ghost):
+      
+        n_splits = (self.arr_map.shape[0]*
+                    self.arr_map.shape[1])//(self.y_dim*10)
+
+        if i == 0 or i == n_blocks -1:
+            n_splits += 1 * fake_ghost
+        else:
+            n_splits += 2 * fake_ghost
+            
+        print(n_splits, block_2d.shape)
         mylist = np.split(block_2d, n_splits)
         block_3d = np.dstack(mylist)
         block_3d = np.rollaxis(block_3d,-1)
@@ -91,22 +99,33 @@ class VoxelProcessing:
         # print("Block_3d Type = ", type(block_3d))
         if operation == 'dilation':
             print("Performing Dilation on block ", i)
-            self.block_dilation(block_3d, self.struct_element)              
+            self.block_dilation(block_3d, i, self.struct_element, 
+                                n_blocks, n_splits, fake_ghost)              
         if operation == 'erosion':
             print("Performing Erosion on block ", i)
             self.block_erosion(block_3d, self.struct_element)
     
-    def block_dilation(self, block_3d, struct_element):
-        dilated = ndimage.grey_dilation(block_3d, structure=struct_element)        
-        dilated.tofile(self.f_handle)
-        # print("Dilated_block size =",  getsizeof(dilated))
-        # print("Block Shape = ", dilated.shape)
+    def block_dilation(self, block_3d, i, struct_element, 
+                       n_blocks, n_splits, fake_ghost):
+
+        dilated = ndimage.grey_dilation(block_3d, structure=struct_element)
+        trimmed = self.trim_ghostCells(dilated, i, n_blocks, n_splits, fake_ghost)            
+        trimmed.tofile(self.f_handle)        
+        print("Block Shape = ", trimmed.shape)
         
     def block_erosion(self, block_3d, struct_element):
         eroded = ndimage.grey_erosion(block_3d, structure=struct_element)
         eroded.tofile(self.f_handle)
-        # print("Block Shape = ", eroded.shape)
-        # print()
+        print("Block Shape = ", eroded.shape)
+        
+    def trim_ghostCells(self, block_3d, i, n_blocks, n_splits, fake_ghost):
+        if i == 0:
+            block_3d = block_3d[0:n_splits-1*fake_ghost,:,:]
+        elif i == n_blocks - 1:
+            block_3d = block_3d[1*fake_ghost:n_splits,:,:]
+        else:
+            block_3d = block_3d[1*fake_ghost:n_splits-1*fake_ghost,:,:]
+        return block_3d 
         
     def merge_blocks(self):
         filename = "output/binary"
