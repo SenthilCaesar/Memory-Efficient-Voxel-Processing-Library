ebf83a8729793aa6ba4a240eb4da68b916e2a999
Senthil Palanivelu <senthilcaesar@yahoo.co.in>
2018-11-08 14:40:58 -0500

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,21 +1,50 @@
+from VoxelProcessing import VoxelProcessing
+import os.path
+import scipy
+from scipy import sparse
 import numpy as np
-from scipy import ndimage
 import datetime
+import time
+import gc
 
-filename='500_700_800_den20.npy'
-structure_element = np.ones((3,3,3))
-merged='output/Merged.npy'
+def runIt(density, dim, blocks, files, structure, operation):
+    for i in range(0, len(density)):
+        filename = str(dim[0]) + "_" + str(dim[1]) + "_" + str(dim[2]) + "_den" + str(int(density[i]*100)) + ".npy"
+        files.append(filename)
+        if not os.path.isfile(filename):
+            data = scipy.sparse.random(dim[0], dim[1]*dim[2], density=density[i], dtype='float64')
+            data = data.toarray()
+            array3d = data.reshape(dim[0],dim[1],dim[2])
+            np.save(filename, array3d)
+            del data, array3d
+            time.sleep(60)
+    for m in range(0, len(files)):
+        for n in range(0, len(blocks)):
+            print("---------------------------------------")
+            print("Filename = ", files[m])
+            print("No of blocks = ", blocks[n])
+            start_t = datetime.datetime.now()
+            data = VoxelProcessing(files[m], dim, structure)
+            arr_2d = data.convert_to_2d()
+            data.compressed_storage(arr_2d)
+            CRS = data.load_compressed()
+            data.get_CRS_mem_size(CRS)
+            data.Morphology(CRS, blocks[n], operation)
+            data.merge_blocks()
+            end_t = datetime.datetime.now()
+            total_t = end_t - start_t
+            print("Time taken = ", total_t.seconds)
+            data.print_memory_usage()
+            gc.collect()
+            del data, CRS, arr_2d
+            time.sleep(60)
 
-start_t = datetime.datetime.now()
-a = np.load(filename)
-a_float32 = np.array(a, dtype='float32')
 
-b = ndimage.grey_dilation(a_float32, structure=structure_element)
-end_t = datetime.datetime.now()
+if __name__ == '__main__':
 
-total_t = end_t - start_t
-print("Time taken = ", total_t.seconds)
-
-Merged = np.load(merged)
-
-print((b==Merged).all())
+    density = [0.10, 0.20, 0.30, 0.40, 0.50]
+    dim = [500,700,800]
+    blocks = [10, 20]
+    files = []
+    structure = np.ones((3,3,3))
+    operation = 'dilation'
