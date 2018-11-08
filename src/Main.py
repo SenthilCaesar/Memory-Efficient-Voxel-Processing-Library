a6fb17d9d29923f44b0902312a7d5261792f512d
Senthil Palanivelu <senthilcaesar@yahoo.co.in>
2018-11-08 14:40:20 -0500

    Update Main.py

--- src/Main.py
+++ src/Main.py
@@ -6,23 +6,17 @@ import datetime
 if __name__ == '__main__':
 
     # Parameters
-    #filename = 'gyroidUniform.npy'
-    #one = np.random.uniform(size=(2000,8000000))
-   # data = scipy.sparse.random(500, 560000, density=0.20, dtype='float64')
-   # data = data.toarray()
-    #one = data.reshape(500,700,800)
-    #np.save('500_700_800_den20.npy', one)
     dimension = [500, 700, 800]
     filename = '500_700_800_den20.npy'
-    #a = np.random.uniform(size=(3,9))
     structure = np.ones((3,3,3))
     operation = 'dilation'
+    n_block = 10
 
     # Main
     start_t = datetime.datetime.now()
     data = VoxelProcessing(filename, dimension, structure)
     arr_2d = data.convert_to_2d()
-    no_of_blocks = data.get_no_of_blocks(arr_2d)
+    no_of_blocks = data.get_no_of_blocks(arr_2d, n_block)
     data.compressed_storage(arr_2d)
     CRS = data.load_compressed()
     data.get_CRS_mem_size(CRS)
@@ -31,3 +25,4 @@ if __name__ == '__main__':
     end_t = datetime.datetime.now()
     total_t = end_t - start_t
     print("Time taken = ", total_t.seconds)
+    data.print_memory_usage()
