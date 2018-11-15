9c1ab5a37233e31adc55570bb78e5728b0dce020
Senthil Palanivelu <senthilcaesar@yahoo.co.in>
2018-11-14 23:16:38 -0500

    Update Main.py

--- src/Main.py
+++ src/Main.py
@@ -7,20 +7,20 @@ if __name__ == '__main__':
 
     # Parameters
     dimension = [500, 700, 800]
-    filename = '500_700_800_den20.npy'
+    filename = '500_700_800_den10.npy'
+    #a = np.random.uniform(size=(3,9))
     structure = np.ones((3,3,3))
-    operation = 'dilation'
+    operation = 'grey_erosion'
     n_block = 10
 
     # Main
     start_t = datetime.datetime.now()
     data = VoxelProcessing(filename, dimension, structure)
     arr_2d = data.convert_to_2d()
-    no_of_blocks = data.get_no_of_blocks(arr_2d, n_block)
     data.compressed_storage(arr_2d)
     CRS = data.load_compressed()
     data.get_CRS_mem_size(CRS)
-    data.Morphology(CRS, no_of_blocks, operation)
+    data.Morphology(CRS, n_block, operation)
     data.merge_blocks()
     end_t = datetime.datetime.now()
     total_t = end_t - start_t
